447565ec602091898d71775ed6b5924042e2d112
nemooo-trash <bam2000000@gmail.com>
2024-05-01 10:37:20 +0300

    add errors

--- sport_events/events/forms.py
+++ sport_events/events/forms.py
@@ -4,12 +4,19 @@ from django.contrib.admin.widgets import AdminSplitDateTime
 from .models import *
 
 class AddEventsForm(forms.ModelForm):
-    date_start = forms.SplitDateTimeField(label="Дата и время проведения спортивного мероприятия", widget=AdminSplitDateTime())
-    date_end = forms.SplitDateTimeField(label="Дата и время окончания спортивного мероприятия", widget=AdminSplitDateTime())
+    date_start = forms.SplitDateTimeField(error_messages={'required': "Обязательные поля дата и время начала спортивного мероприятия"}, label="Дата и время проведения спортивного мероприятия", widget=AdminSplitDateTime())
+    date_end = forms.SplitDateTimeField(error_messages={'required': "Обязательные поля дата и время окончания спортивного мероприятия"},label="Дата и время окончания спортивного мероприятия", widget=AdminSplitDateTime())
 
     class Meta:
         model = Events
         fields = ('city', 'address', 'description', 'date_start', 'date_end', 'type', 'budget')
+
+        error_messages = {
+            'address': {
+                'max_length': "Максимальное количество символов - 255.",
+                'required': "Обязательное поле адрес"
+            },
+        }
 
 
 
